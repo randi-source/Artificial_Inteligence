0f338d8de565320766e34cb8e12a9a8835aecda6
Randi Eka Sanjaya <randi.sanjaya@solusi247.com>
2023-12-31 13:47:52 +0700

    add scraper attempt

---
+++ UAS/README.md
@@ -0,0 +1,11 @@
+## Linkedin Job Description Web Dashboard
+
+This repository create web dashboard using streamlit with web scrapping data from linkedin about Data Scientist. 
+
+Source: \
+https://www.scrapingdog.com/blog/scrape-linkedin-jobs/ \
+https://docs.streamlit.io/
+
+Scrapped Webpage: \
+https://www.linkedin.com/jobs/search?keywords=data%2Bscientist&location=indonesia&trk=public_jobs_jobs-search-bar_search-submit&currentJobId=3786192001&position=1&pageNum=0
+
--- UAS/scraper.py
+++ UAS/scraper.py
@@ -13,22 +13,11 @@ for attempt in range(max_attempts):
         except FileNotFoundError:
             existing_df = pd.DataFrame()
 
-
-        #print("==========EXISTING DF=============")
-        #print(existing_df.head())
-
         new_df = scrape_linkedin_jobs(number_of_jobs=300, exact_match=True)
         new_df = process_dataframe(new_df)
-        print("==========NEW DF=============")
-        print(new_df.head())
-        print(new_df.count())
 
         combined_df = pd.concat([existing_df, new_df], ignore_index=True)
         #combined_df = combined_df.drop_duplicates(subset=['company', 'job_title'])
-
-        print("=========COMBINED DF===========")
-        print(combined_df.head())
-        print(combined_df.count())
 
         combined_df.to_csv('linkedin_jobs.csv', encoding='utf-8', index=False)
         print("Success")
